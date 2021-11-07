0e3f6e33d9b068aea41477282dc44b5436066626
Abdullah Habes <abdulla096@hotmail.com>
2021-11-06 23:08:55 -0400

    implement account transaction depoist withdraw

--- index.js
+++ index.js
@@ -1,29 +1,80 @@
-let balance = 500.00;
+class Account {
+  constructor(name) {
+    this.name = name;
+    this.transactions = [];
+  }
 
-class Withdrawal {
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
 
-  constructor(amount) {
+  get balance() {
+    let total = 0;
+    for (let tx of this.transactions) {
+      total += tx.value;
+    }
+    return total;
+  }
+
+  set balance(amount) {
+    this._balance = amount;
+  }
+}
+
+class Transaction {
+  constructor(account, amount) {
+    this.account = account;
     this.amount = amount;
   }
 
-  commit() {
-    balance -= this.amount;
+  isAllowed() {
+    if (this.constructor.name === 'Withdrawal') {
+      return this.account.balance >= this.amount;
+    }
+    return true;
+
   }
 
+  commit() {
+    if (this.isAllowed()) {
+      this.account.balance += this.value;
+      this.date = new Date();
+      this.account.addTransaction(this);
+    }
+  }
+}
+
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+}
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
 }
 
 
+// ------------------------------------------------------------
+const account = new Account('investments');
+console.log('initial balance', account.balance);
 
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+const deposit = new Deposit(account, 10000000);
+deposit.commit();
+console.log('after deposit', account.balance);
+console.log('Transaction 1:', deposit);
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(account, 50);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Withdrawal(account, 9.99);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Total transactions', account.transactions);
+console.log('Balance:', account.balance);
